da04673ce984b8e8b826d0ee8a5842cca3d22ebb
Dshubn <dima120896@gmail.com>
2025-03-26 21:56:22 +0300

    HW 2 change

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -5,9 +5,9 @@ sizeAll.forEach((element) => {
     element.addEventListener('click', (event) => {
         event.preventDefault()
         sizeAll.forEach((item) => item.classList.remove('font-size_active'))
+        element.classList.add('font-size_active')
         const fontSize = element.dataset.size
         book.classList.remove('book_fs-small', 'book_fs-big')
-
         if(fontSize === 'small') {
             book.classList.add('book_fs-small');
         } else if(fontSize === 'big') {
